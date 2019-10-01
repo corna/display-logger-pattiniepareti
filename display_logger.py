1a757d9fd6afb05132e654a8b401a1bc46f766b4
Nicola Corna <nicola@corna.info>
2019-10-01 07:28:26 +0200

    Add automatic sensor detection

--- display_logger.py
+++ display_logger.py
@@ -3,38 +3,37 @@
 import time
 import urllib.request
 import os
-from os.path import expanduser, isfile
 
-id_out = '28-000003aa674d'
-id_ice = '28-0000052ba923'
+master_out = 'w1_bus_master1'
+master_ice = 'w1_bus_master2'
 
-phant_host = 'data.sparkfun.com'
-public_key = 'xxxxxxxxxxxxxxxxxxxx'
-private_key = 'yyyyyyyyyyyyyyyyyyyy'
-delete_key = 'zzzzzzzzzzzzzzzzzzzz'
-
-phant_failed_file = expanduser("~") + "/displaylogger_phant_failed.txt"
-pep_failed_file = expanduser("~") + "/displaylogger_pep_failed.txt"
+pep_failed_file = os.path.expanduser("~") + "/displaylogger_pep_failed.txt"
 
 sleep_min = 30
 
-phant_url = "https://{}/input/{}?private_key={}&".format(phant_host, public_key, private_key)
 pep_url = "http://www.example.com?"
 
-def gettemp(id):
-  try:  
-    with open('/sys/bus/w1/devices/' + id + '/w1_slave', 'r') as file:
-      line = file.readline()
-      crc = line.rsplit(' ', 1)
-      crc = crc[1].replace('\n', '')
-      if crc == 'YES':
-        line = file.readline()
-        return float(line.rsplit('t=', 1)[1]) / 1000
-      else:
-        return 85
 
-  except Exception:
-    return 85
+def getsensorpath(master):
+    with open(os.path.join('/sys/bus/w1/devices', master, 'w1_master_slaves'), 'r') as f:
+        return f.readline().strip()
+
+
+def gettemp(id):
+    try:
+        w1path = os.path.join('/sys/bus/w1/devices', id, 'w1_slave')
+        with open(w1path, 'r') as file:
+            line = file.readline()
+            crc = line.rsplit(' ', 1)
+            crc = crc[1].replace('\n', '')
+            if crc == 'YES':
+                line = file.readline()
+                return float(line.rsplit('t=', 1)[1]) / 1000
+            else:
+                return 85
+
+    except Exception:
+        return 85
 
 
 def send_data(url, new_request, failed_file):
@@ -42,47 +41,48 @@ def send_data(url, new_request, failed_file):
     success = 0
 
     if os.path.isfile(failed_file):
-      with open(failed_file, 'r') as file:
-        requests = [line.rstrip('\n') for line in file]
+        with open(failed_file, 'r') as file:
+            requests = [line.rstrip('\n') for line in file]
 
     requests.append(new_request)
 
     try:
-      for request in requests:
-        urllib.request.urlopen(url + request)
-        success += 1
-        print('Successfully sent request {}{}'.format(url, request))
+        for request in requests:
+            urllib.request.urlopen(url + request)
+            success += 1
+            print('Successfully sent request {}{}'.format(url, request))
     except urllib.error.HTTPError as error:
-      print('HTTP error while sending {}{}: {}'.format(url, request, error.read()))
+        print('HTTP error while sending {}{}: {}'.format(url, request, error.read()))
     except urllib.error.URLError as error:
-      print('URL error while sending {}{}: {}'.format(url, request, error))
+        print('URL error while sending {}{}: {}'.format(url, request, error))
     except Exception as error:
-      print('Unknown error while sending {}{}'.format(url, request))
+        print('Unknown error while sending {}{}: {}'.format(url, request, error))
 
     still_failing = requests[success:]
 
     if still_failing:
-      if success == 0:
-        with open(failed_file, "a") as file:
-          file.write(requests[-1] + '\n')
-      else:
-        with open(failed_file, "w") as file:
-          for request in still_failing:
-            file.write(request + '\n')
+        if success == 0:
+            with open(failed_file, "a") as file:
+                file.write(requests[-1] + '\n')
+        else:
+            with open(failed_file, "w") as file:
+                for request in still_failing:
+                    file.write(request + '\n')
     else:
-      if os.path.isfile(failed_file):
-        os.remove(failed_file)
+        if os.path.isfile(failed_file):
+            os.remove(failed_file)
 
 
 if __name__ == '__main__':
-  while True:
+    while True:
 
-    current_time = time.localtime()
-    time.sleep((3600 - current_time.tm_min * 60 - current_time.tm_sec - 1) % (sleep_min * 60) + 1)
+        current_time = time.localtime()
+        time.sleep((3600 - current_time.tm_min * 60 - current_time.tm_sec - 1) % (sleep_min * 60) + 1)
 
-    new_request = time.strftime("year=%Y&month=%m&day=%d&hour=%H&minute=%M&") + \
-                  "temp_out={}&temp_ice={}&humidity={}".format(str(gettemp(id_out)), str(gettemp(id_ice)), 0)
+        id_out = getsensorpath(master_out)
+        id_ice = getsensorpath(master_ice)
 
-    send_data(pep_url, new_request, pep_failed_file)
-    #send_data(phant_url, new_request, phant_failed_file)
+        new_request = time.strftime("year=%Y&month=%m&day=%d&hour=%H&minute=%M&") + \
+                                    "temp_out={}&temp_ice={}&humidity={}".format(str(gettemp(id_out)), str(gettemp(id_ice)), 0)
 
+        send_data(pep_url, new_request, pep_failed_file)
